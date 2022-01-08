ee4a202f767874800312cfa6f3be304e41510298
OtterAlexandra <otter9919@mail.ru>
2022-01-08 20:13:05 +0300

    Update tema_moma.py
    combining

--- tema_moma.py
+++ tema_moma.py
@@ -29,6 +29,14 @@ def start_screen():
         pg.display.flip()
 
 
+class Rendering(pg.sprite.Sprite):
+    def __init__(self, pos, name, im, con_size, *group):
+        super().__init__(*group)
+        self.name = name
+        self.image = pg.transform.scale(pg.image.load(im), con_size)
+        self.rect = self.image.get_rect().move(pos)
+
+
 class Symptoms(pg.sprite.Sprite):
     def __init__(self, pos, name, im, *group):
         super().__init__(*group)
@@ -44,6 +52,8 @@ def map_of_world():
     screen.fill('black')
     screen.blit(background_image, (0, 0))
     pg.display.flip()
+    for j in countries:
+        Rendering(*j, countries_group)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -53,6 +63,7 @@ def map_of_world():
                     map_of_symptoms()
 
         pg.display.set_caption('Основной экран')
+        countries_group.draw(screen)
         pg.display.flip()
 
 
@@ -122,6 +133,22 @@ symptoms = [[(454, 300), 'кома', 'симптомы/кома.png'],
             [(734, 300), 'геморрагический шок', 'симптомы/геморрагический шок.png'],
             [(734, 460), 'анемия', 'симптомы/анемия.png']]
 
+countries = [[(750, 410), 'Австралия', 'pictures/Austraalia/australia_0.png', (172.5, 135.8)],
+             [(886, 505.5), 'Новая зеландия', 'pictures/New Zealand/New Zealand_0.png', (87.47, 84.57)],
+             [(848, 359), 'Новая Гвинея', 'pictures/New Guinea/new guinea_0.png', (74.501, 51.428)],
+             [(728, 331.5), 'Иднонезия', 'pictures/Indonesia/indonesia_0.png', (93.569, 65.142)],
+             [(792, 288.5), 'Филиппины', 'pictures/Philippines/philippines_0.png', (47, 42.857)],
+             [(822.3, 175.4), 'Япония', 'pictures/Japan/japan_0.png', (43.902, 71.571)],
+             [(717, 259), 'Ю.-В. Азия', 'pictures/S-E Asia/s-e asia_0.png', (118.847, 114.857)],
+             [(657.5, 199), 'Индия', 'pictures/India/india_0.png', (82.926, 146.142)],
+             [(259.6, 22), 'Гренландия', 'pictures/Greenland/greenland_0.png', (177.383, 147.857)],
+             [(26, 22), 'Канада', 'pictures/Canada/canada_0.png', (302.882, 194.142)],
+             [(570, 402), 'Мадагаскар', 'pictures/Madagascar/madagascar_0.png', (39.467, 47.571)],
+             [(197.7, 289), 'Карибы', 'pictures/Caribbean_Islands/caribbean_0.png', (60.753, 29.571)],
+             [(165, 288), 'Центр. Америка', 'pictures/Central America/central_0.png', (53.215, 68.571)],
+             [(90, 244), 'Мексика', 'pictures/Mexico/mexico_0.png', (111.549, 85.201)]]
+
+countries_group = pg.sprite.Group()
 symptoms_group = pg.sprite.Group()
 
 start_screen()
